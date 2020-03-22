374999a1daf1f78176b846a87014ae05b6c67833
Alexander Whitaker <whitaker.alexander@gmail.com>
2020-03-22 12:27:38 -0500

    move down

--- src/urban_disco/core.clj
+++ src/urban_disco/core.clj
@@ -55,10 +55,24 @@
           (set-current-tile x new-y))
       state)))
 
+(defn move-down [state]
+  (let [grid-height (count (:grid state))
+        y (get-in state [:current-position :y])
+        x (get-in state [:current-position :x])
+        new-y (+ y 1)]
+    (if (< new-y grid-height)
+      (-> state
+          (explore-tile x new-y)
+          (set-current-tile x new-y))
+      state))
+)
+
 (defn move [state direction]
   (case direction
     :up
     (move-up state)
+    :down
+    (move-down state)
     state))
 
 
