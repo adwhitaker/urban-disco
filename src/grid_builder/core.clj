74bc5d5eea1fbbc1579060c2ec17c61af4877646
Alexander Whitaker <whitaker.alexander@gmail.com>
2020-03-31 22:21:08 -0500

    create grid_builder file

---
+++ src/grid_builder/core.clj
@@ -0,0 +1,38 @@
+(ns grid-builder.core
+  (:gen-class))
+
+(defn build-tile [x y grid-height] 
+  {:x        x
+   :y        y
+   :explored false
+   :up       (if (= y 0) :wall :door)
+   :down     (if (= (+ y 1) grid-height) :wall :door)
+   :left     (if (= x 0) :wall :door)
+   :right    (if (= (+ x 1) grid-height) :wall :door)})
+
+(defn build-row [width row-number]
+  (let [row (range width)]
+    (into [] (map #(build-tile % row-number width) row))))
+
+(defn build-grid
+  ([] (build-grid 5))
+  ([height] 
+   (let [grid (range height)]
+     (into [] (map #(build-row height %) grid)))))
+
+(def unexplored-tiles (into [] (flatten (build-grid))))
+
+(defn generate-rooms [grid]
+  (loop [unexplored grid]
+    (if (zero? (count unexplored))
+      unexplored
+      (recur (pop unexplored)))))
+
+
+(def start-tile {
+                 :x 3
+                 :y 4})
+
+(def goal-tile { 
+                :x (rand-int 5) 
+                :y (rand-int 3)})
