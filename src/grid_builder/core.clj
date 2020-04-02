2f4b9699571d4189bc12b5b1d144c014c7300ea8
Alexander Whitaker <whitaker.alexander@gmail.com>
2020-04-01 21:58:10 -0500

    find unexplored neighbors

--- src/grid_builder/core.clj
+++ src/grid_builder/core.clj
@@ -7,12 +7,13 @@
           (> x (- columns 1)) 
           (> y (- columns 1)))
     -1
-    (+ (* x columns) y)))
+    (+ (* y columns) x)))
 
 (defn build-tile [x y grid-height] 
   {:x        x
    :y        y
    :explored false
+   :group    nil
    :up       (if (= y 0) :wall :door)
    :down     (if (= (+ y 1) grid-height) :wall :door)
    :left     (if (= x 0) :wall :door)
@@ -30,6 +31,20 @@
 
 (def unexplored-tiles (into [] (flatten (build-grid))))
 
+(defn nil-group? [tile]
+  (nil? (:group tile)))
+
+(defn unexplored-neighbors [tile grid]
+  (let [top    (index (:x tile)       (- (:y tile) 1) 5)
+        right  (index (+ 1 (:x tile)) (:y tile)       5)
+        bottom (index (:x tile)       (+ 1 (:y tile)) 5)
+        left   (index (- (:x tile) 1) (:y tile)       5)]
+    (reduce (fn [out neighbor-index]
+              (if (and (>= neighbor-index 0) (nil-group? (nth grid neighbor-index)))
+                (conj out (nth grid neighbor-index))
+                out))
+            []
+            [top right bottom left])))
 (defn generate-rooms [grid]
   (loop [unexplored grid]
     (if (zero? (count unexplored))
