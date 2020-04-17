84f29d98181e414dea2a6f1d26fe3655a3180e6c
Alexander Whitaker <whitaker.alexander@gmail.com>
2020-04-17 18:36:09 -0500

    create move module

--- src/urban_disco/core.clj
+++ src/urban_disco/core.clj
@@ -1,15 +1,18 @@
 (ns urban-disco.core
-  (:require [urban-disco.grid.base-grid :as base-grid]
-            [urban-disco.grid.tile      :as tile]
-            [urban-disco.grid.grid      :as grid])
+  (:require [urban-disco.grid.base-grid :refer [build-grid]]
+            [urban-disco.grid.grid      :refer [get-tile]]
+            [urban-disco.grid.core      :refer [generate-grid]]
+            [urban-disco.move.core      :refer [move]])
   (:gen-class))
 
-(def center (grid/center-tile (base-grid/build-grid)))
-(def base-grid (grid/explore (base-grid/build-grid) (:x center) (:y center)))
-(def starting-position center)
+(def base-grid         (build-grid))
+(def starting-tile     (get-tile base-grid 4 2))
+(def goal-tile         (get-tile base-grid 0 3))
+(def game-grid         (generate-grid base-grid starting-tile goal-tile))
+(def starting-position (get-tile game-grid 2 4))
 
 (def state {
-            :grid             base-grid
+            :grid             game-grid
             :current-position starting-position})
 
 (defn -main
