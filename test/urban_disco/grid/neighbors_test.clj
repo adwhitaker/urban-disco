3e25ee12cc45d4c1559c7d20776d912a97fe7890
Alexander Whitaker <whitaker.alexander@gmail.com>
2020-04-18 20:57:36 -0500

    add neighbors test

--- test/urban_disco/grid/neighbors_test.clj
+++ test/urban_disco/grid/neighbors_test.clj
@@ -1,10 +1,27 @@
 (ns urban-disco.grid.neighbors-test
-  (:require [clojure.test   :refer :all]
-            [grid.grid      :refer :all]
-            [grid.base-grid :as    builder]
-            [grid.tile      :as    tile]
-            [grid.neighbors :refer :all]))
+  (:require [clojure.test               :refer :all]
+            [urban-disco.grid.grid      :refer [get-tile]]
+            [urban-disco.grid.base-grid :refer [build-grid]]
+            [urban-disco.grid.tile      :as    tile]
+            [urban-disco.grid.neighbors :refer :all]))
 
 (deftest neighbor-indexes-test
-  (testing "builds indexes"
-    (is (= 13 (:top (neighbor-indexes {:x 3 :y 3}))))))
+  (testing "returns neighbor indexes"
+    (let [{:keys [top bottom left right]} (neighbor-indexes {:x 3 :y 3})]
+      (is (= 13 top))
+      (is (= 23 bottom))
+      (is (= 17 left))
+      (is (= 19 right)))))
+
+(deftest all-unexplored-neighbors-test
+  (let [grid (build-grid)
+        tile (get-tile grid 0 0)
+        unexplored-neighbors (all-unexplored-neighbors grid tile)]
+    (testing "finds all unexplored grid neighbors"
+      (is (= 2 (count unexplored-neighbors)))
+      (let [a (nth unexplored-neighbors 0)
+            b (nth unexplored-neighbors 1)]
+        (is (= 1 (:x a)))
+        (is (= 0 (:y a)))
+        (is (= 0 (:x b)))
+        (is (= 1 (:y b)))))))
